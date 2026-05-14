00f0daeeab7b1d5166fde2ddfc2a110d7024136a
ayushhpatel <ayushpatel162002@gmail.com>
2026-05-14 13:05:10 -0500

    feat: add polished mobile UI and conversational UX

--- packages/app/src/services/api.ts
+++ packages/app/src/services/api.ts
@@ -5,7 +5,7 @@ import { MenuItem } from '../types';
 // Usually for Expo Go on a physical device, this needs to be your network IP
 // e.g. 'http://192.168.1.100:3001/api'
 export const apiClient = axios.create({
-  baseURL: 'https://e11e9447b95941.lhr.life/api',
+  baseURL: 'https://92e2924830dc47.lhr.life/api',
 });
 
 export interface MenuResponse {
